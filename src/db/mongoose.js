b24eb55362878b6376bdc4c751c2297c33db4b96
Isaac lem <isaac.lem@activate.sg>
2019-08-25 22:42:58 +0800

    Update Task

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -5,17 +5,4 @@ const connectionURL = 'mongodb://127.0.0.1:27017/task-manager-api';
 mongoose.connect(connectionURL, {
   useNewUrlParser: true,
   useCreateIndex: true
-})
-
-
-const Task = mongoose.model('Task', {
-  description: {
-    type: String,
-    trim: true,
-    required: true
-  },
-  completed: {
-    type: Boolean,
-    default: false
-  }
 });
