f9de5dd242776c1982890be43732a8d226ef2106
Isaac lem <isaac.lem@activate.sg>
2019-09-07 17:02:28 +0800

    Implemented hashing

--- src/routers/user.js
+++ src/routers/user.js
@@ -2,6 +2,14 @@ const express = require('express');
 const User = require('../models/user');
 const router = new express.Router();
 
+router.post('/users/login', async (req, res) => {
+  try {
+    const user = await User.findByCredentials(req.body.email, req.body.password);
+    res.send(user);
+  } catch (e) {
+    res.status(400).send();
+  }
+});
 
 router.post('/users', async (req, res) => {
   const user = new User(req.body);
@@ -43,13 +51,19 @@ router.patch('/users/:id', async (req, res) => {
   if (!isValidOperation) return res.status(400).send({ error: 'Invalid updates!'});
 
   try {
-    const user = await User.findByIdAndUpdate(
-      req.params.id,
-      req.body,
-      { new: true, runValidators: true }
-    );
-    
+    const user = await User.findById(req.params.id);
+
     if (!user) return res.status(404).send();
+
+    updates.forEach(field => user[field] = req.body[field]);
+
+    await user.save();
+
+    // const user = await User.findByIdAndUpdate(
+    //   req.params.id,
+    //   req.body,
+    //   { new: true, runValidators: true }
+    // );
 
     res.send(user);
   } catch (e) {
